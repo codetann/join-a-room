8ab9273d84b2c90c941c02b052636f87257733dc
codetann <thetannerthomas@gmail.com>
2021-06-12 17:08:14 -0600

    changed production app.get

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.connect(http, (socket) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (res, req) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
